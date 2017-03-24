0f8e2fe6fdbe7b726347127ac47a4906773adf1a
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-03-24 16:46:23 -0500

    updated monthly budgets

--- app/controllers/monthly_budgets_controller.rb
+++ app/controllers/monthly_budgets_controller.rb
@@ -1,4 +1,27 @@
 class MonthlyBudgetsController < ApplicationController
+  before_action :authenticate_user!
   def index
+  end
+
+  def new
+    @monthly_budget = MonthlyBudget.new
+  end
+
+  def create
+    @monthly_budget = MonthlyBudget.new monthly_budget_params
+
+    if @monthly_budget.save
+      flash[:success] = "#{@monthly_budget.month} was successfully created."
+      redirect_to monthly_budget_categories_path(params[:id])
+    else
+      flash[:danger] = "Must enter current month"
+      render :index
+    end
+  end
+
+  private
+
+  def monthly_budget_params
+    params.require(:monthly_budget).permit(:month, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "monthly_budgets#index"
   devise_for :users
   # , controllers: { registrations: 'registrations' }
-
+  post   '/monthly_budgets/:monthly_budget_id/categories', to: 'monthly_budget#create'
   resources :monthly_budgets do
     resources :categories do
       resources :expenses
